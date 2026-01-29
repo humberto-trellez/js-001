582fb2abb1697d0a7529159ffac803e84b5f2408
htrellez <htrellez@icloud.com>
2026-01-28 22:04:24 -0600

    scope

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("Hello, World!!!");
+"use strict";
+
+let height = 200;
+{
+    let weight = 100;
+    {
+        let info = "tall";
+        console.log(height);
+        console.log(weight);
+        console.log(info);
+        var otherVariable = 500;
+    }
+    console.log(height);
+    console.log(weight);
+    //console.log(info);
+}
+console.log(otherVariable);
