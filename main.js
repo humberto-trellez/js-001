c869eb0bae975907bcded6a73a74021b019b704f
htrellez <htrellez@icloud.com>
2026-01-30 15:19:48 -0600

    shadowing

--- main.js
+++ main.js
@@ -1,32 +1,7 @@
-"use strict";
-
-let height = 200;
+let counter = 100;
+console.log(counter);
 {
-    let weight = 100;
-    {
-        let info = "tall";
-        console.log(height);
-        console.log(weight);
-        console.log(info);
-        var otherVariable = 500;
-    }
-    console.log(height);
-    console.log(weight);
-    //console.log(info);
+    let counter = 200;
+    console.log(counter);
 }
-console.log(otherVariable);
-
-var globalGreeting = "Good ";
-
-function greetingFunction() {
-    var localGreeting = " Evening";
-    console.log("function:");
-    console.log(globalGreeting);
-    console.log(localGreeting);
-}
-
-greetingFunction();
-
-console.log("main program:");
-console.log(globalGreeting);
-console.log(localGreeting);
+console.log(counter);
