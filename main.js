54665b5d397f667999f18e90d66524193a57fe8b
htrellez <htrellez@icloud.com>
2026-01-30 15:27:35 -0600

    hoisting

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-let counter = 100;
-console.log(counter);
-{
-    let counter = 200;
-    console.log(counter);
-}
-console.log(counter);
+var height = 100;
+console.log(height);
+console.log(weight);
+var weight = 70;
+console.log(weight);
