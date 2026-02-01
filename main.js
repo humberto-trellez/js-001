93f374908e1f7fdea8d1c0ac9a06fba9efa5376a
htrellez <htrellez@icloud.com>
2026-02-01 17:05:07 -0600

    number type

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
-let isDataValid = true;
-let isStringTooLong = false;
-let isGameOver = false;
-continueLoop = true;
+const year = 1991;
+let delayInSeconds = 0.00016;
+let area = (16 * 3.14);
+let halfArea = area / 2;
 
-console.log(false);
-console.log(typeof false);
-console.log(isDataValid);
-console.log(typeof isDataValid);
+console.log(year);
+console.log(typeof year);
+
+let a = 10;
+let b = 0x10;
+let c = 0o10;
+let d = 0b10;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+
+let x = 9e3;
+let y = 123e-5;
+
+console.log(x);
+console.log(y);
+
+let e = 1 / 0;
+let f = -Infinity;
+
+console.log(e);
+console.log(f);
+console.log(typeof e);
+console.log(typeof f);
+
+let s = "It's definitely not a number";
+let n = s * 10;
+console.log(n);
+console.log(typeof n);
