e16b2d7cc43ea932d9b76a0273b2548a8d78599e
htrellez <htrellez@icloud.com>
2026-02-02 19:52:44 -0600

    undefined type

--- main.js
+++ main.js
@@ -1,29 +1,11 @@
-let country = "Malawi";
-let continent = 'Africa';
+let declaredVar;
+console.log(typeof declaredVar);
 
-console.log(country);
-console.log(typeof country);
-console.log(continent);
-console.log(typeof continent);
+declaredVar = 5;
+console.log(declaredVar);
 
-let sentence = `${country} is located in ${continent}`;
-console.log(sentence);
+declaredVar = undefined;
+console.log(typeof declaredVar);
 
-let message1 = "The vessel 'Mars' called at the port.";
-let message2 = 'Cyclone "Cilida" to pass close to Mauritius.'
-console.log(message1);
-console.log(message2);
-
-let str = "java script language";
-
-console.log(str.length);
-console.log('test'.length);
-
-console.log(str.charAt(0));
-console.log('abc'.charAt(1));
-
-console.log(str.slice(0, 4));
-console.log('test'.slice(1, 3));
-
-console.log(str.split(' '));
-console.log('192.168.1.1'.split('.'));
+console.log(typeof notDeclaredVar);
+console.log(notDeclaredVar);
